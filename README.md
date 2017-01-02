b741520c767006fc9324c185871f82671b7b1909
huangang <huangangzhang@gmail.com>
2017-01-02 15:30:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+# wepy-one
+
+## install
+```bash
+npm install wepy-cli -g
+cd wepy-one
+npm install
+wepy build --watch
+```
+
+#### 项目目录结构
+```
+├── dist                   微信开发者工具指定的目录
+├── node_modules           
+├── src                    代码编写的目录
+|   ├── components         组件文件夹（非完整页面）
+|   |   ├── com_a.wpy      可复用组件 a
+|   |   └── com_b.wpy      可复用组件 b
+|   ├── pages              页面文件夹（完整页面）
+|   |   ├── index.wpy      页面 index
+|   |   └── page.wpy       页面 page
+|   └── app.wpy            小程序配置项（全局样式配置、声明钩子等）
+└── package.json           package 配置
+```
+
+#### 开发使用说明
+1. 使用`微信开发者工具`新建项目，本地开发选择`dist`目录。
+2. `微信开发者工具` --> 项目 --> 关闭ES6转ES5。
+3. 本地项目根目录运行`wepy build --watch`，开启实时编译。
+
+###代码规范：
+1. 变量与方法使用尽量使用驼峰式命名，避免使用`$`开头。
+以`$`开头的方法或者属性为框架内建方法或者属性，可以被使用，使用前请[参考API文档](https://wepyjs.github.io/wepy/#/#api)。
+2. 入口，页面，组件的命名后缀为`.wpy`。外链的文件可以是其它后缀。
+请参考[wpy文件说明](https://wepyjs.github.io/wepy/#/#wpy文件说明)
+3. 使用ES6语法开发。
+框架在ES6下开发，因此也需要使用ES6开发小程序，ES6中有大量的语法糖可以让我们的代码更加简洁高效。
+4. 使用Promise
+框架默认对小程序提供的API全都进行了 Promise 处理，甚至可以直接使用`async/await`等新特性进行开发。
+
+## 感谢
+本程序是代码大量参考了[weapp-one](https://github.com/ahonn/weapp-one),特此声明,表示感谢   
+[ReactNativeOne](https://github.com/lipeiwei-szu/ReactNativeOne)  
+[wepy](https://github.com/wepyjs/wepy)
---
+++ src/api/api.js
@@ -0,0 +1,71 @@
+const host = 'http://v3.wufazhuce.com:8000';
+const wxRequest = (params, url) => {
+  wx.showToast({
+    title: '加载中',
+    icon: 'loading'
+  });
+    console.log(url);
+    wx.request({
+    url: url,
+    method: params.method || 'GET',
+    data: params.data || {},
+    header: {
+      'Content-Type': 'application/json'
+    },
+    complete: (res) => {
+        params.complete && params.complete(res)
+    }
+  }).then(function (res) {
+        if(res.statusCode === 200){
+            wx.hideToast();
+            params.success && params.success(res);
+        }else {
+            params.fail && params.fail(res)
+        }
+    })
+};
+
+// Index
+const getVolById = (params) => wxRequest(params, host + '/api/hp/detail/' + params.query.id);
+const getVolIdList = (params) => wxRequest(params, host + '/api/hp/idlist/0');
+const getVolsByMonth = (params) => wxRequest(params, host + '/api/hp/bymonth/' + params.query.month);
+const getVolDetailById = (params) => wxRequest(params, host + '/api/hp/detail/' + params.query.id);
+
+// Reading
+const getCarousel = (params) => wxRequest(params, host + '/api/reading/carousel');
+const getLastArticles = (params) => wxRequest(params, host + '/api/reading/index');
+const getEssayById = (params) => wxRequest(params, host + '/api/essay/' + params.query.id);
+const getSerialById = (params) => wxRequest(params, host + '/api/serialcontent/' + params.query.id);
+const getQuestionById = (params) => wxRequest(params, host + '/api/question/' + params.query.id);
+const getArticlesByMonth = (params) => {
+  wxRequest(params, host + '/api/' + params.query.type + '/bymonth/' + params.query.month)
+};
+
+// Music
+const getMusicIdList = (params) => wxRequest(params, host + '/api/music/idlist/0');
+const getMusicsByMonth = (params) => wxRequest(params, host + '/api/music/bymonth/' + params.query.month);
+const getMusicDetailById = (params) => wxRequest(params, host + '/api/music/detail/' + params.query.id);
+
+// Movie
+const getMovieListById = (params) => wxRequest(params, host + '/api/movie/list/' + params.query.id);
+const getMovieDetailById = (params) => wxRequest(params, host + '/api/movie/detail/' + params.query.id);
+const getMovieStoryById = (params) => wxRequest(params, host + '/api/movie/' + params.query.id + '/story/1/0');
+
+module.exports = {
+  getVolById,
+  getVolIdList,
+  getVolsByMonth,
+  getVolDetailById,
+  getCarousel,
+  getLastArticles,
+  getEssayById,
+  getSerialById,
+  getQuestionById,
+  getArticlesByMonth,
+  getMusicIdList,
+  getMusicsByMonth,
+  getMusicDetailById,
+  getMovieListById,
+  getMovieDetailById,
+  getMovieStoryById
+};
